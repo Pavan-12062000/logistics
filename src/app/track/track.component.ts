71b1baf75d8d239807a39c57d79971a69088606e
Pavan <ppavansree@gmail.com>
2024-03-06 11:53:13 -0500

    few pages

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-track',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './track.component.css'
 })
 export class TrackComponent {
+  TrackOrder = this.formBuilder.group({
+    trackingid: ['', Validators.required],
+    email: ['', Validators.required, Validators.email],
+  });
+
+  constructor(private formBuilder: FormBuilder){  }
+
+  public counts = ["Order Received", "Order Packed", "Shipment in Progress", "Shipment arrived to Hub","Order Delivered"];
+  public orderStatus = "Order Packed"
+
+  getStatus(trackingid: any){
+    console.log("sdbcvidsvcdfvidfsviojfdviojefiovjdfsiov",trackingid);
+  }
 
 }
